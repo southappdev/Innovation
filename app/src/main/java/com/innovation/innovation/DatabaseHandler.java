0665a3f11960d44c62556c9a6b72a541421e6c82
Kallen Tu <kallentu@hotmail.ca>
2016-08-26 19:35:44 -0700

    Added ReadDatabase function

--- app/src/main/java/com/innovation/innovation/DatabaseHandler.java
+++ app/src/main/java/com/innovation/innovation/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.innovation.innovation;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -74,9 +75,59 @@ public class DatabaseHandler {
 
             //Assigning values, column names are the keys
             ContentValues values = new ContentValues();
-            values.put(DataEntry.COLUMN_NAME_ENTRY_ID, id);
+            values.put(DataEntry.COLUMN_NAME_ENTRY_ID, id); //Columns ""
             values.put(DataEntry.COLUMN_NAME_TITLE, title);
-            values.put(DataEntry.COLUMN_NAME_IMAGE, image);
+            values.put(DataEntry.COLUMN_NAME_IMAGE, image); //Fix
+
+            long newRowId = db.insert(DataEntry.TABLE_NAME, null, values); //Insert row into database, returns primary key value
+        }
+
+        public void ReadDatabase () {
+
+            SQLiteDatabase db = this.getReadableDatabase();
+
+            /**
+             * All variables used in query
+             */
+
+            //What to get
+            //Defines a projection that specifies which columns are needed and will be used
+            String[] projection = {
+                    DataEntry._ID,
+                    DataEntry.COLUMN_NAME_TITLE,
+                    DataEntry.COLUMN_NAME_IMAGE
+            };
+
+            // Filter results WHERE "title" = 'My Title' -> under what to get
+            String selection = DataEntry.COLUMN_NAME_TITLE + " = ?";
+            String[] selectionArgs = { "Jathavan" };
+            // ^What the fuck does this do + if things are fucked up by Jathavan, add "My Title"
+
+            //Order by descending order
+            String sortOrder =
+                    DataEntry.COLUMN_NAME_IMAGE + " DESC";
+
+            //Cursor is a control structure that enables traversal over data records
+            //DeForrest: It is a box that allows access shiz
+            //Query is a set of instructions to describe what data to retrieve and the shape and organization of data
+            //DeForrest: Like a manager, like get dis shit and put on desk
+            Cursor c = db.query(
+                    DataEntry.TABLE_NAME,                     // The table to query
+                    projection,                               // The columns to return
+                    selection,                                // The columns for the WHERE clause
+                    selectionArgs,                            // The values for the WHERE clause
+                    null,                                     // don't group the rows
+                    null,                                     // don't filter by row groups
+                    sortOrder                                 // The sort order
+            );
+
+            /* Use this code in order to move the Cursor, MUST call before reading values
+            cursor.moveToFirst();
+            long itemId = cursor.getLong(
+                cursor.getColumnIndexOrThrow(FeedEntry._ID)
+            );
+             */
+
         }
     }
 }
