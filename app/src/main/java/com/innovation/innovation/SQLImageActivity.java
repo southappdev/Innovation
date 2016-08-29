58fe14b5996b77d177c96687818eb60d63473e78
Kallen Tu <kallentu@hotmail.ca>
2016-08-28 22:33:12 -0700

    Added Button, ImageView, TextViews, getRawEvents(), getEvents()

--- app/src/main/java/com/innovation/innovation/SQLImageActivity.java
+++ app/src/main/java/com/innovation/innovation/SQLImageActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,10 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.IOException;
 import java.sql.SQLClientInfoException;
@@ -28,64 +33,61 @@ public class SQLImageActivity extends ListActivity {
     private ArrayList caption = new ArrayList();
     private ArrayList description = new ArrayList();
     private static DatabaseHandler.DatabaseHelper dataHelp;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     //Called when activity is created
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        populate = (Button) findViewById(R.id.populate); //Insert button that has id populate
+        populate = (Button) findViewById(R.id.populate);
         dataHelp = new DatabaseHandler.DatabaseHelper(this);
 
         SQLiteDatabase db = dataHelp.getWritableDatabase();
 
 
-        Cursor cursors = getRawEvents("select * from gallery"); //Make function later in program
+        Cursor cursors = getRawEvents("select * from gallery");
 
         //If something is returned
         if (cursors.moveToNext()) {
             populate.setVisibility(View.GONE);
             getDataAndPopulate();
-        }
-        else {
+        } else {
             populate.setVisibility(View.VISIBLE);
         }
 
         //What is this - if fucked up, uncomment cause dunno what this does
         //db.delete("gallery", "id=?", new String[]{"12"});
 
-        populate.setOnClickListener(new View.onClickListener(){
+        populate.setOnClickListener(new View.onClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    callInsertion("1", "http://2.bp.blogspot.com/-bkmnlZUKXPs/TjZeTVCgp9I/AAAAAAAAAHI/SPnWJYqq4uQ/s1600/twitter_follow.gif", "First", "This is the first item");
-                    callInsertion("2","http://1.bp.blogspot.com/-HDNFnyRU2Cw/TcuMbBaL70I/AAAAAAAAAGc/7eWN1qnZbAw/s320/seek.JPG","Second","This is the second item");
-                }
-                catch (Exception e){ //No ClientProtocalException
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                callInsertion("1", "http://2.bp.blogspot.com/-bkmnlZUKXPs/TjZeTVCgp9I/AAAAAAAAAHI/SPnWJYqq4uQ/s1600/twitter_follow.gif", "First", "This is the first item");
+                callInsertion("2", "http://1.bp.blogspot.com/-HDNFnyRU2Cw/TcuMbBaL70I/AAAAAAAAAGc/7eWN1qnZbAw/s320/seek.JPG", "Second", "This is the second item");
 
                 getDataAndPopulate();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void callInsertion (String id, String url, String caption, String description) throws Exception, IOException{
+    private void callInsertion(String id, String url, String caption, String description) {
         //Come back to this function
     }
 
 
-    private void insertData (String id, byte[] image, String caption, String description) {
+    private void insertData(String id, byte[] image, String caption, String description) {
         //need to update db with new values
     }
 
-    private void getDataAndPopulate () {
-        //Receiving all the inputed data in order to put in ArrayLists
+    private void getDataAndPopulate() {
+        //Receiving all the input data in order to put in ArrayLists
         id = new ArrayList();
         image = new ArrayList();
         caption = new ArrayList();
@@ -110,44 +112,90 @@ public class SQLImageActivity extends ListActivity {
 
         //Creates new adapter
         ItemsAdapter itemsAdapter = new ItemsAdapter(
-                SQLImageActivity.this, R.layout.item, captionArray); //Need to create id
+                SQLImageActivity.this, R.layout.activity_main, captionArray); //Was R.layout.item
 
         //ListAdapter specifies a layout resource for each row
         setListAdapter(itemsAdapter);
         populate.setVisibility(View.GONE);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SQLImage Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.innovation.innovation/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SQLImage Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.innovation.innovation/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
     private class ItemsAdapter extends BaseAdapter {
         String[] items;
 
-        //Constructor for ItemsAdapter
-        public ItemsAdapter (Context context, int textViewResourceId, String[] items) {
+        //Constructor for ItemsAdapter that gets items from textview
+        public ItemsAdapter(Context context, int textViewResourceId, String[] items) {
             this.items = items;
         }
 
         //This method will be called for every item in the ListView to create views with their properties set as we want.
         @Override
-        public View getView (final int POSITION, View convertView, ViewGroup parent) {
+        public View getView(final int POSITION, View convertView, ViewGroup parent) {
             TextView desc;
             TextView cap;
             View view = convertView;
             ImageView img;
 
             if (view == null) {
-                //LayoutInflater nstantiates a layout XML file into its corresponding View objects.
+                //LayoutInflater instantiates a layout XML file into its corresponding View objects.
                 LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = vi.inflate(R.layout.item, null);
+                view = vi.inflate(R.layout.activity_main, null); //Was R.layout.item
             }
 
-            //Create all these ids
             //Assigns views from the LayoutInflater to their proper view objects
             img = (ImageView) view.findViewById(R.id.image);
             cap = (TextView) view.findViewById(R.id.caption);
             desc = (TextView) view.findViewById(R.id.description);
-            
-            cap.setText(caption.get(POSITION));
-            desc.setText(description.get(POSITION));
-            img.setImageBitmap(BitmapFactory.decodeByteArray(image.get(POSITION), 0, image.get(POSITION).length));
+
+            //cap.setText(caption.get(POSITION));
+            //desc.setText(description.get(POSITION));
+
+            int capPos = (int) caption.get(POSITION);
+            int descPos = (int) description.get(POSITION);
+            int imagePos = (int) image.get(POSITION);
+
+            cap.setText(capPos);
+            desc.setText(descPos);
+            img.setImageBitmap(BitmapFactory.decodeByteArray(imagePos, 0, imagePos.length));
 
             return view;
         }
@@ -165,10 +213,24 @@ public class SQLImageActivity extends ListActivity {
         }
     }
 
-    private Cursor getRawEvents (String sql) {
+    private Cursor getRawEvents(String sql) {
+        SQLiteDatabase db = (dataHelp).getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
 
+        startManagingCursor(cursor); //Find alternative
+        return cursor;
     }
 
+    private Cursor getEvents(String table) {
+        SQLiteDatabase db = (dataHelp).getReadableDatabase();
+
+        //Cursor box -> query = info that is retrieved from the database
+        Cursor cursor = db.query(table, null, null, null, null, null, null);
+
+        startManagingCursor(cursor); //Find alternative
+
+        return cursor;
+    }
 
 
 }
